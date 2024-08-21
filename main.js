685d7af54c775d0def8d662a29ba0a705257165e
Zeinab Hamdy <112763558+ZeinabHamdy@users.noreply.github.com>
2024-08-21 21:56:31 +0300

    added 2 header

---
+++ main.js
@@ -0,0 +1,15 @@
+function swapheaders(){
+    var header1 = document.getElementById("header-1");
+    var header2 = document.getElementById("header-2");
+    setInterval(function(){
+        if(header1.style.display === "block"){
+            header1.style.display = "none";
+            header2.style.display = "block";
+        }else{
+            header1.style.display = "block";
+            header2.style.display = "none";
+        }
+    }, 3000);
+}
+
+swapheaders();
