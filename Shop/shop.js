fa8c566a4c7526f4b1604d32c1ced772a099ebfc
YasmeenFakhri <120497835+YasmeenFakhri@users.noreply.github.com>
2024-08-22 14:53:38 +0300

    shop

---
+++ Shop/shop.js
@@ -0,0 +1,19 @@
+// let price = [...document.querySelectorAll('.price')]
+// let addToCart = [...document.querySelectorAll('.add')]
+// let cards  = [...document.querySelectorAll('.card')]
+
+// for (let i = 0; i < cards.length ; i++) {
+//     cards[i].addEventListener('mouseover' , ()=>{
+//         addToCart[i].style.display = 'block' ;
+//         price[i].style.display = 'none' ;
+
+//     })
+//     cards[i].addEventListener('mouseout' , ()=>{
+//         addToCart[i].style.display = 'none' ;
+//         price[i].style.display = 'block' ;
+
+//     })
+    
+// }
+
+
