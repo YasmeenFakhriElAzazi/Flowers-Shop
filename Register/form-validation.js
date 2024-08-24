96dc040162487adf09d3dbfcb3189658ae1c091b
ZeinabHamdy <zeinabhamdy1010@gmail.com>
2024-08-24 19:29:58 +0300

    updated in validation form in register page

--- Register/form-validation.js
+++ Register/form-validation.js
@@ -1,29 +1,45 @@
 
 document.forms[0].onsubmit = function(event) {
 
-    var form = document.forms[0];
-    var fname= form.fname.value;
-    var lname = form.lname.value;
-    var password = form.password.value;
-    var confirmPassword = form.confirmPassword.value;
-    var email = form.email.value;
-    let valid = true;
+    var form = document.forms[0]
+    , user_name = form.username.value
+    , password = form.password.value
+    , confirmPassword = form.confirmPassword.value
+    , email = form.email.value 
+    , valid = true;
 
-    if(fname === "" || lname === "" || password ==="" || confirmPassword ==="" || email ===""){
+    if(user_name ==="" || password ==="" || confirmPassword ==="" || email ===""){
         valid = false;
-    }
-    
-    if(password !== confirmPassword){
-        valid = false;
-    }
-    if(password.length < 8){
-        valid = false;
-    }
-    
-
-    if(!valid){
-        alert("Please fill out all fields correctly");
+        alert("Please fill out all fields");
         event.preventDefault();
     }
+
+    const isValidEmail = email => {
+        const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return reg.test(String(email).toLowerCase());
+    }
+
+    if(!isValidEmail(email)){
+        valid = false;
+        alert("Please enter a valid email");
+        event.preventDefault();
+    }
+    
+    if(password.length < 8){
+        valid = false;
+        alert("Password must be at least 8 characters");
+        event.preventDefault();
+    }
+
+
+    if(password !== confirmPassword){
+        valid = false;
+        alert("Passwords must be match");
+        event.preventDefault();
+    }
+
+    
 	return valid;
 };
+
+
